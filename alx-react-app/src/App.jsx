e1deb42c1f2150121b2f52e6cc490920be0cdc30
Ikewelugo Chidera <brendanchidera48@gmail.com>
2025-07-12 17:28:56 +0100

    Add UserProfile component and render with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <>
+    <UserProfile name="Brenda" age="22" bio="Frontend developer in the making!" />
       <Header />
       <MainContent />
       <Footer />
