98661717acba1ef77dccbf1ce2c44ffeb682b840
Ikewelugo Chidera <brendanchidera48@gmail.com>
2025-07-19 20:47:14 +0100

    fix MainContent style and complete inline CSS Styling

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
   return (
-    <main style={{ padding: '20px', backgroundColor: '#cce5ff', textAlign: 'center' color: '#001f3f' }}>
-      <p>Welcome to the app!We love touring as much as you do. Explore and enjoy your favorite cities.</p>
+    <main style={{ padding: '20px', backgroundColor: '#cce5ff', textAlign: 'center', color: '#001f3f' }}>
+      <p>Welcome to the app!We love touring as much as you do. Explore and enjoy your favorite cities. I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
