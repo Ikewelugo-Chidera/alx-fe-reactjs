882f331b59af22b2f992b16f391cdf411ec38168
Ikewelugo Chidera <brendanchidera48@gmail.com>
2025-07-19 21:57:50 +0100

    Refactored prop drilling using Context API

---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f0f0f0', textAlign: 'center' }}>
+      <p>I love touring and exploring as much as you do.Welcome to the app! Explore and enjoy your favorite cities.</p>
+    </main>
+  );
+}
+
+export default MainContent;
