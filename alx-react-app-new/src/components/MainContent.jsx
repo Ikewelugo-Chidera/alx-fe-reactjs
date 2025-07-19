7514b90b9d1d107d13114780861b4022ed5625ea
Ikewelugo Chidera <brendanchidera48@gmail.com>
2025-07-19 20:34:17 +0100

    Apply inline CSS styling to all React components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#cce5ff', textAlign: 'center' color: '#001f3f' }}>
+      <p>Welcome to the app!We love touring as much as you do. Explore and enjoy your favorite cities.</p>
+    </main>
+  );
+}
+
+export default MainContent;
