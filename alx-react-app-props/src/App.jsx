407ef96f3339c7ea339a9b7254c578a3d91ad263
Ikewelugo Chidera <brendanchidera48@gmail.com>
2025-07-19 22:20:39 +0100

    correct usercontext.js

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,12 +1,18 @@
-import ProfilePage from './components/ProfilePage';
-import UserContext from './UserContext';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+import UserContext from './UserContext'; // 👈 THIS IS IMPORTANT
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
-      <ProfilePage />
+      <UserProfile />
+      <Header />
+      <MainContent />
+      <Footer />
     </UserContext.Provider>
   );
 }
