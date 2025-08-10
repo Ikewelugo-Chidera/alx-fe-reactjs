381e10d0d7c8716a306456f25db9f762ab9d4cd7
Ikewelugo Chidera <brendanchidera48@gmail.com>
2025-08-10 08:00:52 +0100

    style UserProfile component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from './components/UserProfile'
 
 function App() {
   return (
-    <h1 className="text-4xl text-red-500">Tailwind is working!</h1>
+    <UserProfile />
   )
 }
 
